e5db2692f7f86cd157ec4b9a1e89f11e65fa166b
Michael Cummins <michaelcummins@148-100-189-9.FoxNet.marist.edu>
2016-09-22 14:54:19 -0400

    this is my lab work

--- Labs/2/geometry.java
+++ Labs/2/geometry.java
@@ -1,5 +1,5 @@
 /**
- * file: geometry.java
+ * file: Geometry.java
  * author: Michael Cummins
  * course: CMPT 220
  * assignment: lab 2
@@ -7,14 +7,14 @@
  * version: 1.0
  */
 import java.util.Scanner;
-public class geometry {
+public class Geometry {
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
     System.out.println("What is the length of 1 side of the Pentagon ");
-      int n = input.nextInt();
+    int n = input.nextInt();
     // Calculate the area based on formula
-      double area = (n * Math.pow(n, 2)) / (4 * Math.tan(Math.PI / n));
+    double area = (n * Math.pow(n, 2)) / (4 * Math.tan(Math.PI / n));
     //Print result
-      System.out.println ("Area of regular pentagon is " + area);
+    System.out.println ("Area of regular pentagon is " + area);
   }
 }
