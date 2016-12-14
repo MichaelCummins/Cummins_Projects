5499c4b8d500adddc88d60f5c40c8436fac6a235
Pablo Rivas <pablorp80@gmail.com>
2016-12-13 21:23:26 -0500

    Posting grade and feedback

---
+++ Labs/7/QueueProblem.java
@@ -0,0 +1,18 @@
+public class QueueProblem {
+	/** Main method */
+	public static void main(String[] args) {
+		// Create a Queue object
+		Queue queue = new Queue();
+
+		// Adds 20 numbers from 1 to 20 into the queue
+		for (int i = 1; i <= 20; i++) {
+			queue.enqueue(i);
+		}
+
+		// Removes these numbers and displays them
+		while (!queue.empty()) {
+			System.out.print(queue.dequeue() + " ");
+		}
+		System.out.println();
+	}
+}
